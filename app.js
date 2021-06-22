8488fc032a0967d63fa571ef59819577a3e5e188
viral-bat <51776444+viral-bat@users.noreply.github.com>
2021-06-23 07:29:53 +1000

    updated the app
    Ability to filter the list
    added identifiers for arguments
    command to assign and add task in one line

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ connection.connect(function (err) {
 	console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
 })();
 
-app.command("/add", async ({
+app.command("/bat-add", async ({
 	command,
 	ack,
 	say
@@ -51,78 +51,91 @@ app.command("/add", async ({
 				"channel": command.channel_id,
 				"replace_original": "true",
 				blocks: [{
-						"type": "input",
-						"block_id": 'task_name',
-						"element": {
-							"type": "plain_text_input"
-						},
-						"label": {
+					"type": "context",
+					"elements": [{
 							"type": "plain_text",
-							"text": "Please add task name",
+							"text": "To add task and assign it to yourself: /add |task= Some task name",
+							"emoji": true
+						},
+						{
+							"type": "plain_text",
+							"text": "To add task and assign it: /add |task= Some task name |to= @viral",
+							"emoji": true
+						},
+						{
+							"type": "plain_text",
+							"text": "Make sure to include 'task=' and 'to=' values in given order for above commands",
 							"emoji": true
 						}
-					},
-					{
-						"type": "actions",
-						"elements": [{
-							"type": "button",
-							action_id: 'save_task_name',
-							"text": {
-								"type": "plain_text",
-								"emoji": true,
-								"text": "Save"
-							},
-							"style": "primary",
-							"value": "save_task_name"
-						}]
-					}
-				],
+					]
+				}],
 			});
-		} else {
-			web.chat.postMessage({
-				"text": "Assign task",
-				"channel": command.channel_id,
-				"replace_original": "true",
-				blocks: [{
-						"type": "input",
-						"block_id": "task_name",
-						"label": {
+		} else if (command.text.indexOf('|task=') > -1 && command.text.indexOf('|to=') < 0) {
+			var textArr = command.text.split('|task=');
+			var textTxt = textArr[textArr.length - 1].trim();
+			var sql = `INSERT INTO tasks (taskname, added_by, added_for, added_on, channel_name, status, status_changed_on) 
+			VALUES ('${textTxt}', '${command.user_id}', '${command.user_id}', '${Date.now()}', '${command.channel_name}', 'pending', ${Date.now()})`;
+			console.log(sql);
+			connection.query(sql, function (err, result) {
+				if (err) throw err;
+				web.chat.postMessage({
+					"text": "Task added for you",
+					"channel": command.channel ? command.channel.id : command.channel_id,
+					'blocks': [{
+						"type": "section",
+						"text": {
 							"type": "plain_text",
-							"text": "Task name"
-						},
-						"element": {
-							"type": "plain_text_input",
-							"action_id": "plain_input",
-							"initial_value": `${command.text}`
-						}
-					},
-					{
-						"type": "input",
-						"block_id": 'user_list',
-						"element": {
-							"type": "multi_users_select"
-						},
-						"label": {
-							"type": "plain_text",
-							"text": "Assign task to",
+							"text": "Task added for you :)",
 							"emoji": true
 						}
-					},
-					{
-						"type": "actions",
-						"elements": [{
-							"type": "button",
-							action_id: 'task_ass',
-							"text": {
-								"type": "plain_text",
-								"emoji": true,
-								"text": "Save"
-							},
-							"style": "primary",
-							"value": "assign_task"
-						}]
-					}
-				]
+					}],
+				});
+			});
+		} else if (command.text.indexOf('|task=') > -1 && command.text.indexOf('|to=') > -1) {
+			var textArr = command.text.split('|task=');
+			var commandTxtSplit = textArr[textArr.length - 1];
+			console.log(commandTxtSplit);
+			// check the command order
+			if (commandTxtSplit.indexOf('|to=') < 0) {
+				web.chat.postMessage({
+					"text": "Please make sure to have correct order. Type /add for help",
+					"channel": command.channel ? command.channel.id : command.channel_id,
+				});
+				return;
+			}
+			var textTxt = textArr[textArr.length - 1].split('|to=')[0].trim();
+			var username = textArr[textArr.length - 1].split('|to=')[1].replace('@', '').trim();
+
+			const useridAsync = async () => {
+				const result = await getUserIDByUsername(username);
+				return result;
+			}
+			const userid = await useridAsync();
+
+			if (!userid) {
+				web.chat.postMessage({
+					"text": "No such user :( Please use mention with @",
+					"channel": command.channel ? command.channel.id : command.channel_id,
+				});
+				return;
+			}
+
+			var sql = `INSERT INTO tasks (taskname, added_by, added_for, added_on, channel_name, status, status_changed_on) 
+			VALUES ('${textTxt}', '${command.user_id}', '${userid}', '${Date.now()}', '${command.channel_name}', 'pending', ${Date.now()})`;
+			connection.query(sql, function (err, result) {
+				if (err) throw err;
+				web.chat.postMessage({
+					"text": "Task added",
+					"channel": command.channel ? command.channel.id : command.channel_id,
+					'blocks': [{
+						"type": "section",
+						"text": {
+							"type": "plain_text",
+							"text": `Task added for ${username} :)`,
+							"emoji": true
+						}
+					}],
+				});
 			});
 		}
 	} catch (error) {
@@ -131,204 +144,64 @@ app.command("/add", async ({
 	}
 });
 
-app.action('save_task_name', async ({
-	body,
-	ack,
-	say
-}) => {
-	await ack();
-	var taskname = getValueFromState(body, 'task_name');
-	web.chat.update({
-		"text": "Assign task",
-		'response_type': 'ephemeral',
-		'ts': body.container.message_ts,
-		'channel': body.container.channel_id,
-		'replace_original': true,
-		blocks: [{
-				"type": "input",
-				"block_id": "task_name",
-				"label": {
-					"type": "plain_text",
-					"text": "Task name"
-				},
-				"element": {
-					"type": "plain_text_input",
-					"action_id": "plain_input",
-					"initial_value": `${taskname}`
-				}
-			},
-			{
-				"type": "input",
-				"block_id": 'user_list',
-				"element": {
-					"type": "users_select"
-				},
-				"label": {
-					"type": "plain_text",
-					"text": "Assign task to",
-					"emoji": true
-				}
-			},
-			{
-				"type": "actions",
-				"elements": [{
-					"type": "button",
-					action_id: 'task_ass',
-					"text": {
-						"type": "plain_text",
-						"emoji": true,
-						"text": "Save"
-					},
-					"style": "primary",
-					"value": "assign_task"
-				}]
-			}
-		]
-	})
-});
 
-app.action('task_ass', async ({
-	body,
-	ack,
-	say
-}) => {
-	var obj = body.state.values;
-	var taskname = getValueFromState(body, 'task_name');
-	var assigned_user = getValueFromState(body, 'user_list', 'user_arr');
-	await ack();
-
-	if (!assigned_user) {
-		say({
-			"text": "Please select user",
-			"replace_original": "false",
-		});
-	} else {
-		var sql = `INSERT INTO tasks (taskname, added_by, added_for, added_on, channel_name, status, status_changed_on) 
-	VALUES ('${taskname}', '${body.user.id}', '${assigned_user}', '${Date.now()}', '${body.channel.name}', 'pending', ${Date.now()})`;
-		console.log(sql);
-		connection.query(sql, function (err, result) {
-			if (err) throw err;
-			web.chat.update({
-				"text": "Task added",
-				'response_type': 'ephemeral',
-				'ts': body.container.message_ts,
-				'channel': body.container.channel_id,
-				'replace_original': true,
-				'delete_original': true,
-				'blocks': [{
-					"type": "section",
-					"text": {
-						"type": "plain_text",
-						"text": "Task added :)",
-						"emoji": true
-					}
-				}]
-			});
-		});
-	}
-});
-
-app.command("/list", async ({
+app.command("/bat-list", async ({
 	command,
 	ack,
 	say
 }) => {
-	if (!command.text || command.text == '') {
+	if (command.text.indexOf('|for=') > -1) {
 		try {
 			await ack();
-			var userid = command.user_id;
-			var tasksList = [];
-			var doneTasks = [];
-			var sql = `SELECT * FROM tasks WHERE  
-		added_for="${userid}"`;
-			connection.query(sql, function (err, result) {
-				if (err) throw err;
-				result.forEach(element => {
-					tasksList.push({
-						"value": `${element.taskid}`,
-						"text": {
-							"type": element.status == "done" ? "mrkdwn" : "plain_text",
-							"text": element.status == "done" ? `~${element.taskname}~` : `${element.taskname}`,
-						},
-					});
-				});
-				var perChunk = 10;
 
-				var tasksListInChunk = tasksList.reduce((resultArray, item, index) => {
-					const chunkIndex = Math.floor(index / perChunk)
+			var commandTxtArr = command.text.split('|for=');
+			var commandTxt = commandTxtArr[commandTxtArr.length - 1].trim();
 
-					if (!resultArray[chunkIndex]) {
-						resultArray[chunkIndex] = [] // start a new chunk
-					}
+			var filter = '';
+			var username;
 
-					resultArray[chunkIndex].push(item)
+			if (commandTxt.indexOf('|filter=') > -1) {
+				filter = commandTxt.split('|filter=')[1].trim();
+				username = commandTxt.split('|filter=')[0].trim();
+				console.log(filter, username, '1');
+			} else if (commandTxt.indexOf('|filter=') < 0 && command.text.indexOf('|filter=') > -1) {
+				commandTxtArr = command.text.split('|filter=');
+				commandTxt = commandTxtArr[commandTxtArr.length - 1].trim();
+				filter = commandTxt.split('|for=')[0].trim();
+				username = commandTxt.split('|for=')[1].trim();
+				console.log(filter, username, '2');
+				// web.chat.postMessage({
+				// 	"text": "Follow structure",
+				// 	"channel": command.channel_id,
+				// 	"replace_original": "true",
+				// 	blocks: [{
+				// 		"type": "context",
+				// 		"elements": [{
+				// 				"type": "plain_text",
+				// 				"text": "To list all your tasks, type /list",
+				// 				"emoji": true
+				// 			},
+				// 			{
+				// 				"type": "plain_text",
+				// 				"text": "To list someone else's tasks, type /list |for= @viral",
+				// 				"emoji": true
+				// 			},
+				// 			{
+				// 				"type": "plain_text",
+				// 				"text": "To filter the tasks, type /list |filter= pending|done |for= @viral",
+				// 				"emoji": true
+				// 			}
+				// 		]
+				// 	}],
+				// });
+			} else {
+				var commandTxtArr = command.text.split('|for=');
+				var commandTxt = commandTxtArr[commandTxtArr.length - 1].trim();
+				username = commandTxt;
+				console.log(username, '3');
+			}
 
-					return resultArray
-				}, []);
-
-				tasksListInChunk.forEach((item, index) => {
-					doneTasks[index] = item.filter(task => task.text.type === "mrkdwn");
-				})
-				var blocks = [];
-
-				blocks.push({
-					"type": "section",
-					"block_id": `task_list_header`,
-					"text": {
-						"type": "plain_text",
-						"text": `List of all tasks for <@${command.user_name}>`
-					},
-				});
-
-
-				tasksListInChunk.forEach(function (item, index) {
-					if (doneTasks[index] && doneTasks[index].length > 0) {
-						blocks.push({
-							"type": "section",
-							"block_id": `task_list_${index}_${userid}`,
-							"text": {
-								"type": "plain_text",
-								"text": ' '
-							},
-							"accessory": {
-								"type": "checkboxes",
-								"action_id": "task_status_update",
-								"initial_options": doneTasks[index],
-								"options": tasksListInChunk[index],
-							}
-						});
-					} else {
-						blocks.push({
-							"type": "section",
-							"block_id": `task_list_${index}_${userid}`,
-							"text": {
-								"type": "plain_text",
-								"text": ' '
-							},
-							"accessory": {
-								"type": "checkboxes",
-								"action_id": "task_status_update",
-								"options": tasksListInChunk[index],
-							}
-						});
-					}
-				});
-
-				web.chat.postMessage({
-					"text": "List of all tasks",
-					"channel": command.channel ? command.channel.id : command.channel_id,
-					"text": "List of all tasks",
-					blocks,
-				});
-			});
-		} catch (error) {
-			console.log("err")
-			console.error(error);
-		}
-	} else {
-		try {
-			await ack();
-			var username = command.text.replace('@', '');
+			username = username.replace('@', '');
 			const useridAsync = async () => {
 				const result = await getUserIDByUsername(username);
 				return result;
@@ -347,6 +220,9 @@ app.command("/list", async ({
 			var doneTasks = [];
 			var sql = `SELECT * FROM tasks WHERE  
 			added_for="${userid}"`;
+			if (filter != '' && (filter == 'pending' || filter == 'done')) {
+				sql = sql + ` AND status="${filter}"`;
+			}
 			connection.query(sql, function (err, result) {
 				if (err) throw err;
 				result.forEach(element => {
@@ -382,7 +258,7 @@ app.command("/list", async ({
 					"block_id": `task_list_header`,
 					"text": {
 						"type": "plain_text",
-						"text": `List of all tasks for <@${username}>`
+						"text": `List of all ${filter} tasks for <@${username}>`
 					},
 				});
 
@@ -432,6 +308,168 @@ app.command("/list", async ({
 			console.log("err")
 			console.error(error);
 		}
+	} else {
+		try {
+			await ack();
+			var userid = command.user_id;
+			var tasksList = [];
+			var doneTasks = [];
+			var filter = '';
+			if (command.text.indexOf('|filter=') > -1) {
+				var commandTxtArr = command.text.split('|filter=');
+				filter = commandTxtArr[commandTxtArr.length - 1].trim();
+			}
+			var sql = `SELECT * FROM tasks WHERE  
+		added_for="${userid}"`;
+			if (filter.length > 0) {
+				sql = sql + ` AND status="${filter}"`;
+			}
+			connection.query(sql, function (err, result) {
+				if (err) throw err;
+				result.forEach(element => {
+					tasksList.push({
+						"value": `${element.taskid}`,
+						"text": {
+							"type": element.status == "done" ? "mrkdwn" : "plain_text",
+							"text": element.status == "done" ? `~${element.taskname}~` : `${element.taskname}`,
+						},
+					});
+				});
+				var perChunk = 10;
+
+				var tasksListInChunk = tasksList.reduce((resultArray, item, index) => {
+					const chunkIndex = Math.floor(index / perChunk)
+
+					if (!resultArray[chunkIndex]) {
+						resultArray[chunkIndex] = [] // start a new chunk
+					}
+
+					resultArray[chunkIndex].push(item)
+
+					return resultArray
+				}, []);
+
+				tasksListInChunk.forEach((item, index) => {
+					doneTasks[index] = item.filter(task => task.text.type === "mrkdwn");
+				})
+				var blocks = [];
+
+				blocks.push({
+					"type": "section",
+					"block_id": `task_list_header`,
+					"text": {
+						"type": "plain_text",
+						"text": `List of all ${filter} tasks for <@${command.user_name}>`
+					},
+				});
+
+
+				tasksListInChunk.forEach(function (item, index) {
+					if (doneTasks[index] && doneTasks[index].length > 0) {
+						blocks.push({
+							"type": "section",
+							"block_id": `task_list_${index}_${userid}`,
+							"text": {
+								"type": "plain_text",
+								"text": ' '
+							},
+							"accessory": {
+								"type": "checkboxes",
+								"action_id": "task_status_update",
+								"initial_options": doneTasks[index],
+								"options": tasksListInChunk[index],
+							}
+						});
+					} else {
+						blocks.push({
+							"type": "section",
+							"block_id": `task_list_${index}_${userid}`,
+							"text": {
+								"type": "plain_text",
+								"text": ' '
+							},
+							"accessory": {
+								"type": "checkboxes",
+								"action_id": "task_status_update",
+								"options": tasksListInChunk[index],
+							}
+						});
+					}
+				});
+
+				web.chat.postMessage({
+					"text": "List of all tasks",
+					"channel": command.channel ? command.channel.id : command.channel_id,
+					"text": "List of all tasks",
+					blocks,
+				});
+			});
+		} catch (error) {
+			console.log("err")
+			console.error(error);
+		}
+	}
+});
+
+app.command("/batbox-help", async ({
+	command,
+	ack,
+	say
+}) => {
+	try {
+		await ack();
+		web.chat.postMessage({
+			"text": "Follow structure",
+			"channel": command.channel_id,
+			"replace_original": "true",
+			blocks: [{
+					"type": "header",
+					"text": {
+						"type": "plain_text",
+						"text": "Need some Bathelp?",
+						"emoji": true
+					}
+				},
+				{
+					"type": "context",
+					"elements": [{
+							"type": "plain_text",
+							"text": "To add task and assign it to yourself: /add |task= Some task name",
+							"emoji": true
+						},
+						{
+							"type": "plain_text",
+							"text": "To add task and assign it: /add |task= Some task name |to= @viral",
+							"emoji": true
+						},
+						{
+							"type": "plain_text",
+							"text": "Make sure to include 'task=' and 'to=' values in given order for above commands",
+							"emoji": true
+						},
+						{
+							"type": "plain_text",
+							"text": "To list all your tasks, type /list. Can be used as /list |filter= done or /list |filter= pending",
+							"emoji": true
+						},
+						{
+							"type": "plain_text",
+							"text": "To list someone else's tasks, type /list |for= @viral",
+							"emoji": true
+						},
+						{
+							"type": "plain_text",
+							"text": "To filter the tasks, type /list |filter= pending|done |for= @viral",
+							"emoji": true
+						}
+					]
+				}
+			],
+		});
+
+	} catch (error) {
+		console.log("err")
+		console.error(error);
 	}
 });
 
@@ -618,4 +656,101 @@ async function getUserIDByUsername(username) {
 // 	console.log('running a task every minute');
 // }, {
 // 	scheduled: true,
+// });
+
+// app.action('save_task_name', async ({
+// 	body,
+// 	ack,
+// 	say
+// }) => {
+// 	await ack();
+// 	var taskname = getValueFromState(body, 'task_name');
+// 	web.chat.update({
+// 		"text": "Assign task",
+// 		'response_type': 'ephemeral',
+// 		'ts': body.container.message_ts,
+// 		'channel': body.container.channel_id,
+// 		'replace_original': true,
+// 		blocks: [{
+// 				"type": "input",
+// 				"block_id": "task_name",
+// 				"label": {
+// 					"type": "plain_text",
+// 					"text": "Task name"
+// 				},
+// 				"element": {
+// 					"type": "plain_text_input",
+// 					"action_id": "plain_input",
+// 					"initial_value": `${taskname}`
+// 				}
+// 			},
+// 			{
+// 				"type": "input",
+// 				"block_id": 'user_list',
+// 				"element": {
+// 					"type": "users_select"
+// 				},
+// 				"label": {
+// 					"type": "plain_text",
+// 					"text": "Assign task to",
+// 					"emoji": true
+// 				}
+// 			},
+// 			{
+// 				"type": "actions",
+// 				"elements": [{
+// 					"type": "button",
+// 					action_id: 'task_ass',
+// 					"text": {
+// 						"type": "plain_text",
+// 						"emoji": true,
+// 						"text": "Save"
+// 					},
+// 					"style": "primary",
+// 					"value": "assign_task"
+// 				}]
+// 			}
+// 		]
+// 	})
+// });
+
+// app.action('task_ass', async ({
+// 	body,
+// 	ack,
+// 	say
+// }) => {
+// 	var obj = body.state.values;
+// 	var taskname = getValueFromState(body, 'task_name');
+// 	var assigned_user = getValueFromState(body, 'user_list', 'user_arr');
+// 	await ack();
+
+// 	if (!assigned_user) {
+// 		say({
+// 			"text": "Please select user",
+// 			"replace_original": "false",
+// 		});
+// 	} else {
+// 		var sql = `INSERT INTO tasks (taskname, added_by, added_for, added_on, channel_name, status, status_changed_on) 
+// 	VALUES ('${taskname}', '${body.user.id}', '${assigned_user}', '${Date.now()}', '${body.channel.name}', 'pending', ${Date.now()})`;
+// 		console.log(sql);
+// 		connection.query(sql, function (err, result) {
+// 			if (err) throw err;
+// 			web.chat.update({
+// 				"text": "Task added",
+// 				'response_type': 'ephemeral',
+// 				'ts': body.container.message_ts,
+// 				'channel': body.container.channel_id,
+// 				'replace_original': true,
+// 				'delete_original': true,
+// 				'blocks': [{
+// 					"type": "section",
+// 					"text": {
+// 						"type": "plain_text",
+// 						"text": "Task added :)",
+// 						"emoji": true
+// 					}
+// 				}]
+// 			});
+// 		});
+// 	}
 // });
